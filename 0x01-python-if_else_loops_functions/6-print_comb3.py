90eb9417fcab53eff3f8e4c3026faeef25a2abac
blindlit0 <blindlit0@gmail.com>
2024-01-02 19:13:48 -0800

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ numbers = 0
 while numbers <= 89:
     if numbers % 10 == 0:
         numbers += 1 + numbers // 10
-print("{:02d}".format(numbers), end='\n' if numbers == 89 else ", ")
-numbers += 1
+    print("{:02d}".format(numbers), end='\n' if numbers == 89 else ", ")
+    numbers += 1
