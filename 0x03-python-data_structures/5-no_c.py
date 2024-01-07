403ec643a15fe05f14d0ddd2fc4dbec6815c83ec
blindlit0 <blindlit0@gmail.com>
2024-01-06 16:30:33 -0800

    tuples

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    newStr = ""
+    for char in my_string:
+        if (char != 'c' and char != 'C'):
+            newStr += char
+    return (newStr)
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+Lists, I know LIsts more!!
